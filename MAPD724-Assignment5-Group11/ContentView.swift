5d95d1a98e8cf886b310e1f022066563832c3d71
Cole Anam <coleanam2000@gmail.com>
2024-04-13 17:24:09 -0400

    Initial Commit

---
+++ MAPD724-Assignment5-Group11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MAPD724-Assignment5-Group11
+//
+//  Created by Cole Anam on 13/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MAPD724-Assignment5-Group11/MAPD724_Assignment5_Group11App.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD724_Assignment5_Group11App.swift
+//  MAPD724-Assignment5-Group11
+//
+//  Created by Cole Anam on 13/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MAPD724_Assignment5_Group11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
